b89b70e02745c5ef598ab8e57eb812b6ec6519cb
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-29 16:41:14 +0800

    Add project files.

---
+++ ucp1_pemdasss/ucp1_pemdasss.cpp
@@ -0,0 +1,28 @@
+//1.contoh deklarasi intejer
+// Variabel Integer:umur 
+// Variabel String: nama
+// Variabel Float : tinggi_badan
+
+//2.Prosedur (Procedure):
+//def greet_user(name):
+
+//3, p = Point(10, 20) membuat objek point
+
+
+//4. prosedur 
+// def greet_user(name):
+// fungsi
+// def calculate_square(number):
+
+//5. contoh looping untuk mencetak angka dari 1 hingga 5
+// for i in range(1, 6):
+// print(i)
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
